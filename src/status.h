82a2fa87b151516ab7bb1bc271a04e160f84693b
0902-Sharon <sharonthomas.cs20@rvce.edu.in>
2022-01-19 09:29:36 +0530

    fifth commit

--- src/status.h
+++ src/status.h
@@ -2,8 +2,9 @@
 #define STATUS_H
 
 #define PIXIEOS_ALL_OK 0
-#define EIO 1
-#define EINVARG 2
-#define ENOMEM 3
-#define EBADPATH 4
+#define EIO 1       // I/O error
+#define EINVARG 2   // invalid argument error
+#define ENOMEM 3    //No memory
+#define EBADPATH 4   // Bad Call
+#define EFSNOTUS 5   //Not from the filesystem
 #endif
